1455a492b12412b97f71395edd86ac020308d5a5
Weblure <Weblurejoltik@gmail.com>
2023-11-07 16:05:37 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype HTML>
+<HTML>
+    <head>
+        <title>GPTFish - Comic Translation Suite</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+        <!-- Add support for iOS devices -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <!-- Add support for Android devices -->
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="theme-color" content="#ffffff">
+        <title></title>
+        <!-- Link to your CSS file -->
+        <link rel="stylesheet" href="style.css?v=2">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <style>
+        .material-symbols-sharp {
+            font-variation-settings:
+            'FILL' 1,
+            'wght' 400,
+            'GRAD' 0,
+            'opsz' 40
+        }
+    </style>
+    </head>
+
+    <body>
+
+        <main>
+            
+        </main>
+        
+    </body>
+
+</HTML
