061d6c333cb35757259e0e9f12bca01f8bf83660
Weblure <Weblurejoltik@gmail.com>
2023-11-07 19:03:18 -0700

    kermit

--- index.php
+++ index.php
@@ -3,16 +3,21 @@
     <head>
         <title>GPTFish - Comic Translation Suite</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+
+        <!-- Generic mobile support (DISABLED)-->
+        <!-- <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no, minimal-ui"> -->
+
         <!-- Add support for iOS devices -->
-        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="no">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <!-- Add support for Android devices -->
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="theme-color" content="#ffffff">
-        <title></title>
-        <!-- Link to your CSS file -->
-        <link rel="stylesheet" href="style.css?v=2">
+        <meta name="mobile-web-app-capable" content="no">
+        <meta name="theme-color" content="#333333">
+
+        <!-- CSS links -->
+        <link rel="stylesheet" href="styles/master.css">
+        <link rel="stylesheet" href="styles/fish-ui.css">
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
@@ -31,7 +36,27 @@
     <body>
 
         <main>
-            
+
+            <div id="topBarContainer">
+
+            </div>
+
+            <div id="masterContentContainer">
+
+                <div id="imageContainer">
+
+                </div>
+
+                <div id="masterTranslationContainer">
+
+                    <div id="leftTranslationContainer"></div>
+
+                    <div id="rightTranslationContainer"></div>
+
+                </div>
+
+            </div>
+
         </main>
         
     </body>
